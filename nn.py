85771a63af6dcd0b7eb3cc2f2b6c056b524cb2ec
Jeff Schulz <jeffschulz7@gmail.com>
2019-09-17 01:09:07 -0500

    stuck on deltas

--- nn.py
+++ nn.py
@@ -37,18 +37,30 @@ y = np.array([[1,0],
               [1,0],
               [1,0],
               [1,0]
-             ])
+              ])
 
 # weights with random numbers
-h1_n1_w  = [.1,.2,.3, .2]
-h1_n2_w  = [.1,.1,.1, .1]
-h1_n3_w  = [.3,.3,.3, .9]
-h2_n1_w  = [ 0, 0, 0,  0]
-h2_n2_w  = [.1,.1,.1, .2]
-h2_n3_w  = [.1,.1,.1,  0]
-h2_n4_w  = [.2,.2,.2,-.1]
-out_n1_w = [1.5,1.2, 1,0,-.2]
-out_n2_w = [  0, .8,.1,0,-.1]
+h1_weights  =   np.array([[.1, .2, .3],
+                          [.1, .1, .1],
+                          [.3, .3, .3]
+                          ])
+
+h1_bias     =   np.array([[.2], [.1], [.9]])
+
+h2_weights  =   np.array([[ 0,  0,  0],
+                          [.1, .1, .1],
+                          [.1, .1, .1],
+                          [.2, .2, .2]
+                          ])
+
+h2_bias     =   np.array([[0], [.2], [0], [-.1]])
+
+out_weights =   np.array([[1.5, 1.2,  1, 0],
+                          [  0,  .8, .1, 0]
+                          ])
+
+out_bias    =   np.array([-.2, -.1])
+
 
 # print('hidden layer 1, neuron 1 weights')
 # print(n1_w)
@@ -60,12 +72,11 @@ out_n2_w = [  0, .8,.1,0,-.1]
 ###############################################
 # Epochs
 ###############################################
-eta = 3.0 # learning rate
+eta = 5.0 # learning rate
 err_break = 0.001 # stop when below this error
-max_epoch = 20000 # how many epochs? (each epoch will run through all 4 data points)
+max_epoch = 10 # how many epochs? (each epoch will run through all 4 data points)
 err = np.zeros((max_epoch,1)) # lets record error to plot (get a convergence plot)
-end_index = max_epoch-1 # what index did we stop on?
-inds = np.asarray([0,1,2,3]) # array of our 4 indices (data point index references)
+
 for k in range(max_epoch): 
     
     # init error
@@ -73,85 +84,65 @@ for k in range(max_epoch):
     
     # random shuffle of data each epoch?
     #inds = np.random.permutation(inds)
-    
-    # doing online, go through each point, one at a time
-    for i in range(4): 
-        
-        # what index?
-        inx = inds[i]
-        
-        # forward pass
-        # layer 1
-        v = np.ones((3, 1))
-        v[0] = np.dot(X[inx,:], n1_w) # neuron 1 fires (x as input)
-        v[0] = sigmoid(v[0])        # neuron 1 sigmoid
-        v[1] = np.dot(X[inx,:], n2_w) # neuron 2 fires (x as input)
-        v[1] = sigmoid(v[1])    
-        # layer 2
-        oo = np.dot(np.transpose(v), n3_w) # neuron 3 fires, taking neuron 1 and 2 as input
-        o = sigmoid(oo) # hey, result of our net!!!
-        
-        # error
-        err[k] = err[k] + ((1.0/2.0) * np.power((y[inx] - o), 2.0))
-                
-        # backprop time folks!!!
-        
-        # output layer, our delta is (delta_1 * delta_2)
-        delta_1 = (-1.0) * (y[inx] - o)
-        delta_2 = sigmoid(o,derive=True) # note how I called it, I passed o=sigmoid(oo)
-        
-        # now, lets prop it back to the weights
-        delta_ow = np.ones((3, 1))
-        # format is
-        #  delta_index = (input to final neuron) * (Err derivative * Sigmoid derivative)
-        delta_ow[0] = v[0]  *  (delta_1*delta_2)
-        delta_ow[1] = v[1]  *  (delta_1*delta_2)
-        delta_ow[2] = v[2]  *  (delta_1*delta_2)
-        
-        # neuron n1
-        delta_3 = sigmoid(v[0],derive=True)
-        # same, need to prop back to weights
-        delta_hw1 = np.ones((3, 1))
-        # format
-        #              input     this Sig der     error from output   weight to output neuron
-        delta_hw1[0] = X[inx,0]  *  delta_3  *  ((delta_1*delta_2)   *n3_w[0])
-        delta_hw1[1] = X[inx,1]  *  delta_3  *  ((delta_1*delta_2)   *n3_w[0])
-        delta_hw1[2] = X[inx,2]  *  delta_3  *  ((delta_1*delta_2)   *n3_w[0])     
-        
-        # neuron n2
-        delta_4 = sigmoid(v[1],derive=True)
-        # same, need to prop back to weights        
-        delta_hw2 = np.ones((3, 1))
-        delta_hw2[0] = X[inx,0]  *  delta_4  *   ((delta_1*delta_2)   *n3_w[1])
-        delta_hw2[1] = X[inx,1]  *  delta_4  *   ((delta_1*delta_2)   *n3_w[1])
-        delta_hw2[2] = X[inx,2]  *  delta_4  *   ((delta_1*delta_2)   *n3_w[1])
-        
-        # update rule
-        n1_w = n1_w - eta * delta_hw1 # neuron 1 in hidden layer 1
-        n2_w = n2_w - eta * delta_hw2 # neuron 2 in hidden layer 1
-        n3_w = n3_w - eta * delta_ow  # neuron 1 in hidden layer 2
-        
-    if( err[k] < err_break ):
-        end_index = k
-        break
+            
+    # forward pass
+    # layer 1
+    v1 = np.dot(X, np.transpose(h1_weights)) + h1_bias # h1
+    v1 = sigmoid(v1)
+    # v1 is (8,3)  
 
-print('Ran ' + str(end_index) + ' iterations')
+    # layer 2
+    v2 = np.dot(v1, np.transpose(h2_weights)) + h2_bias # h2
+    v2 = sigmoid(v2)
+    # v2 is (8,4) 
+
+    # output layer
+    oo = np.dot(v2, np.transpose(out_weights)) + out_bias # out
+    o  = sigmoid(oo) # hey, result of our net!!!
+    # o is (8,2)
+    
+    # error
+    err[k] = np.sum(((1.0/2.0) * np.power((y - o), 2.0)))
+            
+    # backprop time folks!!!
+    
+    # output layer, our delta is (delta_1 * delta_2)
+    delta_1 = (-1.0) * (y - o) #(8,2)
+    delta_2 = sigmoid(o,derive=True) # note how I called it, I passed o=sigmoid(oo) # (8,2)
+    
+    # now, lets prop it back to the weights
+    #delta_ow = np.ones((8, 3, 1))
+    
+    # format is
+    #  delta_index  =         input to final neuron    . (Err derivative * Sigmoid derivative)
+    #(4,2)          =         (8,4)T                   . (8,2)      
+    delta_ow        = np.dot( np.transpose(v2)         , (delta_1        * delta_2)                   )
+
+    # Layer 2
+    delta_4 = sigmoid(v2,derive=True) #(8,4)
+    #error_l =         input            this Sig der        error from output         weight to output neuron
+    #(4,3)   =        [(8,4)          * (8,4)]T     .    [ (8,2)          .           (4,2)T           ]
+    #delta_h2 = np.dot(np.transpose(v1 * delta_4) , np.dot(delta_1*delta_2 , np.transpose(out_weights)) )
+    delta_h2 = np.dot(np.transpose(delta_4) , np.dot(delta_1*delta_2 , np.transpose(out_weights)) )
+    
+    #(4,3)      (8,4)                     (8,2)     (4,2)T
+    delta_h2 = sigmoid(v2,derive=True) * delta_1*delta_2 . np.transpose(out_weights)
+    
+    # Layer 1
+    delta_3 = sigmoid(v1,derive=True) #(8,3)
+    #error_l =         input           this Sig der       error from output          weight to output neuron
+    #(3,3)   =        [(8,3)         * (8,3)]T     .    [ (8,4)          .           (3,4)T           ]
+    #delta_h1 = np.dot(np.transpose(X * delta_3) , np.dot(delta_4         , np.transpose(h2_weights)) )
+    delta_h1 = np.dot(np.transpose(delta_3) , np.dot(delta_4         , np.transpose(h2_weights)) )
+
+    # update rule
+    h1_weights  = h1_weights  - eta * delta_h1 #hidden layer 1
+    h2_weights  = h2_weights  - eta * delta_h2 #hidden layer 2
+    out_weights = out_weights - eta * delta_ow #output layer
+    
 
 # plot it        
-plt.plot(err[0:end_index])
+plt.plot(err)
 plt.ylabel('error')
 plt.xlabel('epochs')
 plt.show()
-        
-# what were the values (just do forward pass)  
-for i in range(4): 
-    
-    # forward pass
-    v = np.ones((3, 1))
-    v[0] = np.dot(X[i,:], n1_w)
-    v[0] = sigmoid(v[0])
-    v[1] = np.dot(X[i,:], n2_w)
-    v[1] = sigmoid(v[1])    
-    oo = np.dot(np.transpose(v), n3_w)
-    o = sigmoid(oo) 
-    print(str(i) + ": produced: " + str(o) + " wanted " + str(y[i]))
