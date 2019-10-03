19b53f086744aeb35043f06d6e905092bda2c9cc
Jeff Schulz <jeffschulz7@gmail.com>
2019-10-03 15:18:24 -0500

    not done fixing sanity check

--- nn.py
+++ nn.py
@@ -54,7 +54,7 @@ h1_weights  =   np.array([[.1, .2, .3],
                           [.3, .3, .3]
                           ])
 
-h1_bias     =   np.array([[.2], [.1], [.9]])
+h1_bias     =   np.array([.2, .1, .9])
 
 h2_weights  =   np.array([[ 0,  0,  0],
                           [.1, .1, .1],
@@ -62,68 +62,72 @@ h2_weights  =   np.array([[ 0,  0,  0],
                           [.2, .2, .2]
                           ])
 
-h2_bias     =   np.array([[0], [.2], [0], [-.1]])
+h2_bias     =   np.array([0, .2, 0, -.1])
 
 out_weights =   np.array([[1.5, 1.2,  1, 0],
                           [  0,  .8, .1, 0]
                           ])
 
-out_bias    =   np.array([[-.2], [-.1]])
-
+out_bias    =   np.array([-.2, -.1])
 
 do_plot     = True
 eta         = .01    # learning rate
 max_epoch   = 1     # how many epochs? (each epoch will run through all 4 data points)
 err         = np.zeros((max_epoch,1))   # lets record error to plot (get a convergence plot)
-
+v1          = np.ones((3,1))
+v2          = np.ones((4,1))
+o           = np.ones((2,1))
 
 ###############################################
 # Epochs
 ###############################################
-
 for k in range(max_epoch): 
     
     # init error
     err[k] = 0    
 
+    for i,x in enumerate(data):
+        ## forward pass
+        for j in range(3):
+            # layer 1 -- v1 is (3,1), x is (1,3), h1_w is (3,3) 
+            v1[j] = np.dot(x, np.transpose(h1_weights[j])) + h1_bias[j] # h1
+            v1[j] = tanh(v1[j])
 
-    ## forward pass
-    # layer 1 -- v1 is (8,3) 
-    v1 = np.dot(X, np.transpose(h1_weights)) + h1_bias.T # h1
-    v1 = tanh(v1)
+        for j in range(4):
+            # layer 2 -- v2 is (4,1), v1 is (3,1), h2_w is (4,3) 
+            v2[j] = np.dot(np.transpose(v1), np.transpose(h2_weights[j])) + h2_bias[j] # h2
+            v2[j] = tanh(v2[j])
 
-    # layer 2 -- v2 is (8,4) 
-    v2 = np.dot(v1, np.transpose(h2_weights)) + h2_bias.T # h2
-    v2 = tanh(v2)
-
-    # output layer -- o is (8,2)
-    oo = np.dot(v2, np.transpose(out_weights)) + out_bias.T # out
-    o = oo
-    
-    # error
-    err[k] = np.sum(((1.0/2.0) * np.power((y - o), 2.0))) / 8
+        for j in range(2):
+            # output layer -- o is (2,1), v2 is (4,1), out_w is (2,4)
+            o[j] = np.dot(np.transpose(v2), np.transpose(out_weights[j])) + out_bias[j] # out
+            o[j] = o[j]
+        
+        # error
+        # y is (8,2), o is (2,1)
+        err[k] += np.sum(((1.0/2.0) * np.power((y[i] - o.T), 2.0)))
 
 
-    ## backprop
-    # output layer -- (8,2)
-    #delta_ow = (-1.0) * (y - o) * tanh(o,derive=True)
-    delta_ow     = (-1.0) * (y - o)
+        ## backprop
+        # output layer -- (8,2)
+        #delta_ow = (-1.0) * (y - o) * tanh(o,derive=True)
+        delta_ow     = (-1.0) * (y - o)
 
-    # Layer 2
-    #(8,4)             (8,2)      (2,4)           (8,4)
-    delta_h2     = np.dot(delta_ow , out_weights) * tanh(v2,derive=True)
-    
-    # Layer 1
-    #(8,3)             (8,4)      (4,3)           (8,3)
-    delta_h1     = np.dot(delta_h2 , h2_weights) * tanh(v1,derive=True)
+        # Layer 2
+        #(8,4)             (8,2)      (2,4)           (8,4)
+        delta_h2     = np.dot(delta_ow , out_weights) * tanh(v2,derive=True)
+        
+        # Layer 1
+        #(8,3)             (8,4)      (4,3)           (8,3)
+        delta_h1     = np.dot(delta_h2 , h2_weights) * tanh(v1,derive=True)
 
-    # update rule
-    h1_weights  = h1_weights  - np.transpose( eta * np.dot(np.transpose(X) , delta_h1) ) #hidden layer 1
-    h1_bias     = h1_bias - np.transpose( eta * np.dot(np.ones((1,8)) , delta_h1) )
-    h2_weights  = h2_weights  - np.transpose( eta * np.dot(np.transpose(v1) , delta_h2) ) #hidden layer 2
-    h2_bias     = h2_bias - np.transpose( eta * np.dot(np.ones((1,8)) , delta_h2) )
-    out_weights = out_weights - np.transpose( eta * v2.T.dot( delta_ow ) ) #output layer
-    out_bias    = out_bias - np.transpose( eta * np.dot(np.ones((1,8)), delta_ow))
+        # update rule
+        h1_weights  = h1_weights  - np.transpose( eta * np.dot(np.transpose(X) , delta_h1) ) #hidden layer 1
+        h1_bias     = h1_bias - np.transpose( eta * np.dot(np.ones((1,8)) , delta_h1) )
+        h2_weights  = h2_weights  - np.transpose( eta * np.dot(np.transpose(v1) , delta_h2) ) #hidden layer 2
+        h2_bias     = h2_bias - np.transpose( eta * np.dot(np.ones((1,8)) , delta_h2) )
+        out_weights = out_weights - np.transpose( eta * v2.T.dot( delta_ow ) ) #output layer
+        out_bias    = out_bias - np.transpose( eta * np.dot(np.ones((1,8)), delta_ow))
     
 
 print("\n\nHidden Layer 1 Weights:\n\n",h1_weights)
