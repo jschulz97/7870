b78a70008eb98f6125456bb714f411ab003276bd
Jeff Schulz <jeffschulz7@gmail.com>
2019-11-05 18:32:35 -0600

    started. dunno what to do?

--- Fuzzy/fuzzy.py
+++ Fuzzy/fuzzy.py
@@ -1,2 +1,20 @@
 import numpy as numpy
 from matplotlib import pyplot as plt
+
+class Fuzzy:
+    def __init__(self,):
+        print('init')
+    
+    def rule1(self,x):
+        if(x < .25):
+            return 'low'
+        elif(x >= .25 and x <= .75):
+            return 'medium'
+        elif(x > .75):
+            return 'high'
+
+
+
+if(__name__ == '__main__'):
+    fuz = Fuzzy()
+    print(fuz.rule1(.3))
